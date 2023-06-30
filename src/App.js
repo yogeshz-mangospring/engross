f578e4d0a693624d301a1671ae8d5825238fe766
yogeshz-mangospring <yogeshz@mangospring.com>
2023-06-30 11:04:01 +0530

    final app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { useState } from "react";
 
 function App() {
 
-  const [counter, setCounter] = useState({ min: 1, sec: 2 });
+  const [counter, setCounter] = useState({ min: 0, sec: 0 });
   const [clear, setClear] = useState();
   const [flag, setFlag] = useState(false);
   const [reverseCounter, Setreverse] = useState();
   const [reverseFlag, setReversFlag] = useState(false);
 
-  if(counter.sec === 0 && counter.min === 0){
+  if (counter.sec === 0 && counter.min === 0) {
     clearInterval(reverseCounter);
   }
 
@@ -16,7 +16,7 @@ function App() {
     setClear(
       setInterval(() => {
         setCounter((prevCounter) => ({
-          min: prevCounter.sec === 59 ? (prevCounter.min + 1) : prevCounter.min ,
+          min: prevCounter.sec === 59 ? (prevCounter.min + 1) : prevCounter.min,
           sec: prevCounter.sec === 59 ? counter.sec = 0 : prevCounter.sec + 1
         }));
       }, 1000)
@@ -33,7 +33,7 @@ function App() {
       setInterval(() => {
         setCounter((prevCounter) => ({
           min: prevCounter.sec === 60 ? (prevCounter.min - 1) : prevCounter.min,
-          sec: prevCounter.sec === 0 ? prevCounter.sec = 60 : prevCounter.sec -1,
+          sec: prevCounter.sec === 0 ? prevCounter.sec = 60 : prevCounter.sec - 1,
         }))
       }, 1000)
     );
@@ -59,10 +59,10 @@ function App() {
         <h1>Engross</h1>
         <h3 className="mt-4">{counter.min === 0 ? "00" : counter.min < 10 ? `0${counter.min}` : counter.min} : {counter.sec === 0 ? "00" : counter.sec < 10 ? `0${counter.sec}` : counter.sec}</h3>
         <div className="mt-4">
-          <button className={`btn btn-success me-2 ${(flag && counter.min !== 0) ? 'disabled' : ''}`} onClick={start}>Start</button>
-          <button className={`btn btn-dark me-2 ${(counter.sec === 0 || reverseFlag) ? 'disabled' : ''}`} onClick={reverse}>Reverse</button>
-          <button className={`btn btn-danger me-2 ${counter.sec === 0 && 'disabled'}`} onClick={pause}>Pause</button>
-          <button className={`btn btn-warning ${counter.sec === 0 && 'disabled'}`} onClick={reset}>Reset</button>
+          <button className={`btn btn-success me-2 ${(flag && (counter.min !== 0 || counter.sec !== 0)) ? 'disabled' : ''}`} onClick={start}>Start</button>
+          <button className={`btn btn-dark me-2 ${((counter.min === 0 && counter.sec === 0) || reverseFlag) ? 'disabled' : ''}`} onClick={reverse}>Reverse</button>
+          <button className={`btn btn-danger me-2 ${(counter.min === 0 && counter.sec === 0) && 'disabled'}`} onClick={pause}>Pause</button>
+          <button className={`btn btn-warning ${(counter.min === 0 && counter.sec === 0) && 'disabled'}`} onClick={reset}>Reset</button>
         </div>
       </div>
     </div>
